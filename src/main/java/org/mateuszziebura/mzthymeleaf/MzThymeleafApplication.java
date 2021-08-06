fc5ff2c142883612aa0a3ee6b3b3f17f62ba53ca
Mateusz Ziebura <mateusz_860@op.pl>
2021-08-06 19:37:51 +0200

    Initialize project

---
+++ src/main/java/org/mateuszziebura/mzthymeleaf/MzThymeleafApplication.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.mzthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MzThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mateuszziebura/mzthymeleaf/MzThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.mzthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
