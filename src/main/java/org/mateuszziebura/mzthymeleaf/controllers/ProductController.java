7a91cdc5a19979adb1fce022d3b528cdabc46081
Mateusz Ziebura <mateusz_860@op.pl>
2021-08-09 16:27:33 +0200

    Returning Objects to Thymeleaf

--- src/main/java/org/mateuszziebura/mzthymeleaf/controllers/ProductController.java
+++ src/main/java/org/mateuszziebura/mzthymeleaf/controllers/ProductController.java
@@ -1,13 +1,27 @@
 package org.mateuszziebura.mzthymeleaf.controllers;
 
+import org.mateuszziebura.mzthymeleaf.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/product")
     public String product(){
+        return "redirect:/index";
+    }
+    @RequestMapping("/product/{id}")
+    public String getProduct(@PathVariable Integer id, Model model){
+        model.addAttribute("product", productService.getProduct(id));
         return "product";
     }
 }
