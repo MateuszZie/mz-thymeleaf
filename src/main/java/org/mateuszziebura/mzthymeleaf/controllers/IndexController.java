45506f8806f171d706f3d1bfb5543ebd93132281
Mateusz Ziebura <mateusz_860@op.pl>
2021-08-09 21:05:39 +0200

    Spring MVC Configuration

--- src/main/java/org/mateuszziebura/mzthymeleaf/controllers/IndexController.java
+++ src/main/java/org/mateuszziebura/mzthymeleaf/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package org.mateuszziebura.mzthymeleaf.controllers;
 
+import org.mateuszziebura.mzthymeleaf.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private final ProductService productService;
+
+    public IndexController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("products", productService.listProducts());
         return "index";
     }
 }
