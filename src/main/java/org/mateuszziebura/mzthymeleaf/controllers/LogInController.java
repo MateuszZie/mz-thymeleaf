38cd95b0338362c361c1b29e739198af62c8c611
Mateusz Ziebura <mateusz_860@op.pl>
2021-08-18 13:20:18 +0200

    Login Form

---
+++ src/main/java/org/mateuszziebura/mzthymeleaf/controllers/LogInController.java
@@ -0,0 +1,31 @@
+package org.mateuszziebura.mzthymeleaf.controllers;
+
+import org.mateuszziebura.mzthymeleaf.domain.Loged;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class LogInController {
+
+
+    @RequestMapping("/login")
+    public String logIn(Model model){
+
+        model.addAttribute("loged", new Loged());
+        return "LogIn";
+    }
+    @PostMapping("/success")
+    public String successLog(@Valid Loged loged, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "LogIn";
+        }
+        return "checkoutcomplete";
+    }
+}
